88dba25b3b5b4983e1784519958cf74d08bd1b29
Richard Moss <richard@englishtutorslive.com>
2021-02-05 14:14:29 +0000

    Namespace prod classNames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClasName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClasName={generateClasName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
