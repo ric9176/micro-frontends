e51a9cefb6a1a15606fc847719bf73955d201c1e
Richard Moss <richard@englishtutorslive.com>
2021-02-07 21:10:17 +0000

    sort routing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createMemoryHistory } from 'history';
+
 import App from './App';
 
-const mount = el => {
-  ReactDom.render(<App />, el);
+// Mount function to start app
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDom.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = history.location;
+      if (nextPathname !== pathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
+
+// In dev env we call mount immediately
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.getElementById('_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
